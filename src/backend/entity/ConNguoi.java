d3503bac9512ddfdb42bdc410781cd5970fee584
Trần Thảo Nương <6551071060@st.utc2.edu.vn>
2026-04-20 19:28:34 +0700

    fix thuoc tinh

--- src/backend/entity/ConNguoi.java
+++ src/backend/entity/ConNguoi.java
@@ -1,66 +1,60 @@
 package backend.entity;
 
 public abstract class ConNguoi {
-    private String id;
-    private String hoTen;
-    private String sdt;
-    private String diaChi;
-    private String email;
+    private String ID;
+    private String HoTen;
+    private String Sdt;
+    private String DiaChi;
+    private String Email;
 
     // ================ CONSTRUCTOR =================
     public ConNguoi(){}
 
-    public ConNguoi(String id, String hoTen, String sdt, String diaChi, String email) {
-        this.id = id;
-        this.hoTen = hoTen;
-        this.sdt = sdt;
-        this.diaChi = diaChi;
-        this.email = email;
+    public ConNguoi(String ID, String HoTen, String Sdt, String DiaChi, String Email) {
+        this.ID = ID;
+        this.HoTen = HoTen;
+        this.Sdt = Sdt;
+        this.DiaChi = DiaChi;
+        this.Email = Email;
     }
 
     // ================ HÀM CHỨC NĂNG =================
     public abstract void hienThiVaiTro();
 
     // ================ GET =================
-    public String getId() {
-        return id;
-    }
+    
+	public String getID() {
+		return ID;
+	}
+	public String getHoTen() {
+		return HoTen;
+	}
+	public String getSdt() {
+		return Sdt;
+	}
+	public String getDiaChi() {
+		return DiaChi;
+	}
+	public String getEmail() {
+		return Email;
+	}
 
-    public String getHoTen() {
-        return hoTen;
-    }
-
-    public String getSdt() {
-        return sdt;
-    }
-
-    public String getDiaChi() {
-        return diaChi;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    // ================ SET =================
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public void setDiaChi(String diaChi) {
-        this.diaChi = diaChi;
-    }
-
-    public void setSdt(String sdt) {
-        this.sdt = sdt;
-    }
-
-    public void setHoTen(String hoTen) {
-        this.hoTen = hoTen;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
+	// ================ SET =================
+	
+	public void setID(String iD) {
+		ID = iD;
+	}
+	public void setHoTen(String hoTen) {
+		HoTen = hoTen;
+	}
+	public void setSdt(String sdt) {
+		Sdt = sdt;
+	}
+	public void setDiaChi(String diaChi) {
+		DiaChi = diaChi;
+	}
+	public void setEmail(String email) {
+		Email = email;
+	}
+    
 }
