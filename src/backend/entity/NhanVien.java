404349e058ba082a123682762bb534bc5bb27d16
Trần Thảo Nương <6551071060@st.utc2.edu.vn>
2026-04-20 20:43:49 +0700

    fix thuoc tinh

--- src/backend/entity/NhanVien.java
+++ src/backend/entity/NhanVien.java
@@ -1,45 +1,55 @@
 package backend.entity;
 
 public class NhanVien extends ConNguoi {
-    private double luong;
-    private String chucVu;
-
+    private double Luong;
+    private String ChucVu;
+    private boolean TrangThai;
     // Constructor rỗng
     public NhanVien() {
         super();
     }
 
     // ================ CONSTRUCTOR =================
-    public NhanVien(String id, String hoTen, String sdt, String diaChi, String email, double luong, String chucVu) {
-        super(id, hoTen, sdt, diaChi, email);
-        this.luong = luong;
-        this.chucVu = chucVu;
+    public NhanVien(String ID, String HoTen, String Sdt, String DiaChi, String Email, double Luong, String ChucVu, boolean TrangThai) {
+        super(ID, HoTen, Sdt, DiaChi, Email);
+        this.Luong = Luong;
+        this.ChucVu = ChucVu;
+        this.TrangThai = TrangThai;
     }
 
     // ============== HÀM CHỨC NĂNG ===============
     @Override
     public void hienThiVaiTro() {
         System.out.println("VAI TRÒ: Nhân viên hệ thống");
-        System.out.println("Chức vụ: " + this.chucVu);
+        System.out.println("Chức vụ: " + this.ChucVu);
+        System.out.println("Trạng thái làm việc: " + this.TrangThai);
     }
 
     // ================ GETTER =================
 
     public double getLuong() {
-        return luong;
+        return Luong;
     }
 
     public String getChucVu() {
-        return chucVu;
+        return ChucVu;
     }
 
+    public boolean isTrangThai() {
+		return TrangThai;
+	}
     // ================ SETTER =================
 
-    public void setLuong(double luong) {
-        this.luong = luong;
+	public void setLuong(double Luong) {
+        this.Luong = Luong;
     }
 
-    public void setChucVu(String chucVu) {
-        this.chucVu = chucVu;
+    public void setChucVu(String ChucVu) {
+        this.ChucVu = ChucVu;
     }
+
+	public void setTrangThai(boolean trangThai) {
+		TrangThai = trangThai;
+	}
+    
 }
