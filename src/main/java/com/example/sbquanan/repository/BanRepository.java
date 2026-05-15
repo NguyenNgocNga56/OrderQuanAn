6390519c49f87821926a96f13d7cd02c2227a00e
ttn238 <6551071060@st.utc2.edu.vn>
2026-05-15 10:37:44 +0700

    xoa file thua

--- src/main/java/com/example/sbquanan/repository/BanRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.sbquanan.repository;
-
-import com.example.sbquanan.entity.Ban;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BanRepository extends JpaRepository<Ban, Long> {}
